45bd3425e57926106cf508ce01ca654260c555a5
Beatriz Martins <beatriz.salgueiromartins@gmail.com>
2018-10-23 11:44:56 +0000

    Alterações em Páginas - Commit feito por João Pedro

--- footer.php
+++ footer.php
@@ -1,10 +1,17 @@
-        <div class="rowdape">
-            <div class="rodape">
-                <p>Site desenvolvido por BB&JJ</p>
-            </div>
-        </div>   
-       
-       
-        <?php wp_footer();?>
-    </body>
+<div class="row">
+    <div class="col-md-12 rodape">
+        <p>ETEC DE PRAIA GRANDE <br>
+
+SEDE: AVENIDA GUADALAJARA, 941 - GUILHERMINA - PRAIA GRANDE - SP TELEFONE: (13) 3491-3153 E (13) 3491-1585 <br>
+
+EXTENSÃO: AVENIDA DOUTOR ROBERTO DE ALMEIDA VINHAS, 10.119 - BALNEÁRIO MARACANÃ - PRAIA GRANDE - SP TELEFONE: (13) 3471-2395
+<br><br><br>
+Site desenvolvido po BB&JJ</p>
+    </div>
+</div>
+
+
+<?php wp_footer();?>
+
+</body>   
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php get_header(); ?>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="jumbotron">
-                <h2>Seja bem vindo ao meu novo tema</h2>
-            </div>
-        </div>
-    </div>
-    
+ 
     <div class="row slider">
         <div class="col-md-12">
             <?php masterslider(2); ?>
@@ -20,11 +13,11 @@
             while ( have_posts() ) {
                 the_post();
                 $cont++;
-            if ($cont <= 3){
+            if ($cont <= 6){
      ?>
      <div class="col-md-4">
      <div class="card">
-  <img src="<?php the_post_thumbnail (); ?>
+  <div class="img-responsive img-thumbnail foto"><?php the_post_thumbnail(); ?></div>
   <div class="container">
  
     <h4 ><b><?php the_title(); ?></b></h4> 
@@ -33,12 +26,7 @@
   </div>
   </div>
 </div>
-          <!-- <div class="col-md-4"> <div class="img-responsive img-thumbnail foto"><?php the_post_thumbnail (); ?></div>
-               <h3><?php the_excerpt(); ?></h3>
-              
-               <p><php the_excerpt(); ?></p>
-               <a class="btn btn-primary" href="<?php the_permalink();?>">Leia mais </a>
-            </div>   -->
+    
             
 <?php
 }}}
